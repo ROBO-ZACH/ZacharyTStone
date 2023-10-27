f662b986251cb7b1adfd8fa95c52a34a6250e4cd
Zach Stone <zach.stone.developer@gmail.com>
2023-10-27 08:54:33 -0400

    test adding a date with worker

--- .github/scripts/update-readme.js
+++ .github/scripts/update-readme.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const axios = require("axios");
 const readmePath = "README.md";
 
+const getCurrentDateTime = () => {
+  const now = new Date();
+  return `${now.toDateString()} ${now.toLocaleTimeString()}`;
+};
+
 const getRandomQuote = async () => {
   try {
     const response = await axios.get("https://api.quotable.io/random");
@@ -16,11 +21,14 @@ const getRandomQuote = async () => {
 const updateReadme = async () => {
   const quote = await getRandomQuote();
   if (quote) {
+    const currentDateTime = getCurrentDateTime();
     const readmeContent = fs.readFileSync(readmePath, "utf-8");
-    const updatedReadme = readmeContent.replace(
-      /"(.|\n)*"— (.*)/,
-      `"${quote}"`
-    );
+    const updatedReadme = readmeContent
+      .replace(/"(.|\n)*"— (.*)/, `"${quote}"`)
+      .replace(
+        /auto-magically updated at: (.*)/,
+        `auto-magically updated at: ${currentDateTime}`
+      );
     fs.writeFileSync(readmePath, updatedReadme);
   }
 };
