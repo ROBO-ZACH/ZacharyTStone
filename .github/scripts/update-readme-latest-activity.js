8cc3c0e3b0338d9789ebbfabbfea6492b9dfab09
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-10-29 12:32:54 -0400

    update latest activity

---
+++ .github/scripts/update-readme-latest-activity.js
@@ -0,0 +1,106 @@
+const fs = require("fs");
+const axios = require("axios");
+const readmePath = "./README.md";
+require("dotenv").config();
+const { execSync } = require("child_process");
+
+const ONLY_LOGIN_TO_CHECK = "ZacharyTStone";
+
+const getLatestEvent = async () => {
+  const myFollowerURL = "https://api.github.com/users/ZacharyTStone/events";
+
+  try {
+    const response = await axios.get(myFollowerURL);
+    const LatestEvents = response.data;
+
+    // sort the array by created_at date
+
+    const sortedEvents = LatestEvents.sort((a, b) => {
+      if (a.created_at && b.created_at) {
+        return new Date(b.created_at) - new Date(a.created_at);
+      }
+    }).filter((event) => {
+      return event?.actor?.login === ONLY_LOGIN_TO_CHECK;
+    });
+
+    const LatestEvent = sortedEvents[0];
+
+    console.log("LatestEvent", LatestEvent);
+
+    return LatestEvent;
+  } catch (error) {
+    console.error("Error fetching latest event:", error.message);
+    return null;
+  }
+};
+
+const updateReadme = async () => {
+  const event = await getLatestEvent();
+
+  console.log("event", event);
+
+  if (event) {
+    console.log("Updating README with latest event..");
+
+    const repoName = event.repo.name;
+    const repoURL = event.repo.url;
+    const time = new Date(event.created_at).toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    });
+
+    if (!repoName || !repoURL || !time) {
+      console.error("Error parsing event data:", event);
+      return;
+    }
+
+    const readmeContent = fs.readFileSync(readmePath, "utf-8");
+
+    // Use a regular expression to find and replace the blockquote content
+    const updatedReadme = readmeContent.replace(
+      /🤖 Zach recently worked on (.*)/,
+      `🤖 Zach recently worked on [${repoName}](${repoURL}) at ${time}`
+    );
+
+    console.log("updatedReadme", updatedReadme);
+    fs.writeFileSync(readmePath, updatedReadme);
+
+    const gitUserEmail = process.env.GIT_USER_EMAIL;
+    const gitUserName = process.env.GIT_USER_NAME;
+
+    if (!gitUserEmail || !gitUserName) {
+      console.error(
+        "Git user email or name not provided in environment variables."
+      );
+      return;
+    }
+
+    execSync(`git config user.email "${gitUserEmail}"`);
+    execSync(`git config user.name "${gitUserName}"`);
+
+    // if the user has changed then commit and push the changes
+
+    if (updateReadme === readmeContent) {
+      console.log("No changes to the README. Exiting...");
+      return;
+    }
+
+    // commit the changes
+    console.log("Committing updated README...");
+    const commitMessage = `Update README with new repo activity by zach on ${repoName} at ${time}`;
+    const commitCommand = `git commit -am "${commitMessage}"`;
+    const commitOutput = execSync(commitCommand, { stdio: "inherit" });
+    console.log(commitOutput);
+
+    // push the changes
+    console.log("Pushing updated README...");
+    const pushOutput = execSync("git push", { stdio: "inherit" });
+    console.log(pushOutput);
+
+    console.log("README update complete!");
+
+    // return the updated readme
+    return updatedReadme;
+  }
+};
+
+updateReadme();
